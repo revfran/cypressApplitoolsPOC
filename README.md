ced5d148cbce083cdda77fe9495d1b572cc63b04
revfran <2746521+revfran@users.noreply.github.com>
2021-01-30 10:09:09 +0100

    Added Visual tests

--- README.md
+++ README.md
@@ -8,14 +8,33 @@ Second step is to add Browserstack as target device
 Web site targeted was a sample book store https://demoqa.com/books
 
 
-# How to develop
+# How to develop tests
 
-` yarn cypress open`
+1. Set APPLITOOLS_API_KEY variable (GitHub account required to register)
+
+2. ` yarn cypress open`
+
+
+# How to run tests
+
+1. Set APPLITOOLS_API_KEY variable (GitHub account required to register)
+
+2. `yarn cypress run --spec "cypress/integration/book_store.spec.js"`
+
+
+# Sample reports
+
+Sample execution reports are available in [reports](./reports).
+
+Video was obtained running with --record option
+
+Captures from https://eyes.applitools.com
 
 # Primary references
 
 - https://www.learnwithjason.dev/visual-testing-using-cypress-and-applitools
 
+- https://www.npmjs.com/package/@applitools/eyes-cypress
 
 # Other references
 
--- cypress/integration/book_store.spec.js
+++ cypress/integration/book_store.spec.js
@@ -1,15 +1,37 @@
 /// <reference types="cypress" />
 
-describe('book_store functional tests',() => {
+describe('book_store functional tests', () => {
 
-it('Search a title', () => {
+    beforeEach(() => {
 
-    cy.visit('https://demoqa.com/books')
+        cy.eyesOpen({
+            appName: 'Hello World!',
+            testName: 'My first JavaScript test!',
+            browser: { width: 1024, height: 860 },
+        });
+    })
 
-    cy.get('#searchBox').type('git{enter}')
 
-    cy.get('div[class="action-buttons"]').should('have.length', 1)
+    it('Search a title', () => {
 
-    cy.get('span[id="see-book-Git Pocket Guide"]').click()
-})
+        cy.visit('https://demoqa.com/books')
+        cy.eyesCheckWindow('Main Page');
+
+        // Baseline
+        cy.get('#searchBox').type('git{enter}')
+
+        // Uncomment to force a visual test failure
+        //cy.get('#searchBox').clear().type('git Pocket{enter}')
+
+        cy.get('div[class="action-buttons"]').should('have.length', 1)
+        cy.eyesCheckWindow('Filtered');
+
+        cy.get('span[id="see-book-Git Pocket Guide"]').click()
+        cy.eyesCheckWindow('Specific book');
+    })
+
+    afterEach(() => {
+        cy.eyesClose()
+    })
+
 })
